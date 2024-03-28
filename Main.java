5896369d71aee36d9a4b75955eff0842915667d2
edanu <edanurgider94@hotmail.com>
2024-03-28 23:57:49 +0300

    28.03.2024      23:57

---
+++ Main.java
@@ -0,0 +1,56 @@
+public class Main {
+
+    public static  void main (String [] args) {
+
+        String a = "Welcome";
+        String b = " Hello World ";
+
+        String toplam = "Toplam:" + a + b;
+        System.out.println(toplam);
+
+
+
+        String x = "Merhaba";
+        int g = 35;
+
+        String m = x + g;
+
+        System.out.println(m);
+
+        String w = "Merhaba";
+        double ı = 3.52;
+        byte  c = 10;
+
+        w = w + ı + c;
+
+        System.out.println(w);
+
+        char p = '?';
+        String r = "Welcome , how r u " + p ;
+        System.out.println(r);
+
+        String h = "Java" +
+                " PRogramlama " +
+                " Dili";
+
+        System.out.println(h);
+
+
+        //    \t yapınca 1 tab  bosluk bırakıyor
+
+        String n = " Java\tProgramlama\tDili";
+        System.out.println(n);
+
+        //   \n  yapınca yeni satıra gecer. Alt alta yazar.
+
+        String eda = "Eda\nGideros";
+        System.out.println(eda);
+
+
+
+
+
+
+
+    }
+}
